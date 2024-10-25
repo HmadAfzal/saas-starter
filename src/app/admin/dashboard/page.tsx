853f5684d180f6da2493d22d0014be67150638ad
Hmad Afzal <hmadafzal00@gmail.com>
2024-10-25 12:57:22 +0500

    auth added

---
+++ src/app/admin/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { SignOutButton } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+ 
+  return (
+    <>
+    <SignOutButton>ndnd</SignOutButton>
+    </>
+  )
+}
+
+export default page
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { SignOutButton } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <SignOutButton>ndnd</SignOutButton>
+    
+    </div>
+  )
+}
+
+export default page
